dd09e26133f4dbfbcc67a27374949a841d6c9d42
Jason R. Coombs <jaraco@jaraco.com>
2015-02-24 20:08:10 -0600

    Use setuptools_scm

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ sphinx = ['sphinx'] if needs_sphinx else []
 
 setup_params = dict(
 	name='jaraco.ui',
-	use_hg_version=True,
+	use_scm_version=True,
 	author="Jason R. Coombs",
 	author_email="jaraco@jaraco.com",
 	description="jaraco.ui",
@@ -28,7 +28,7 @@ setup_params = dict(
 	install_requires=[
 	],
 	setup_requires=[
-		'hgtools',
+		'setuptools_scm',
 	] + pytest_runner + sphinx,
 	tests_require=[
 		'pytest',
