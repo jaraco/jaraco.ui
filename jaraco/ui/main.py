547cec62c234e3c1c704148c0359b1eeb7d4ae3d
Jason R. Coombs <jaraco@jaraco.com>
2024-08-15 22:00:16 -0400

    Add docstring for main.

--- jaraco/ui/main.py
+++ jaraco/ui/main.py
@@ -3,6 +3,18 @@ import typer
 
 
 def main(func, app=typer.Typer(add_completion=False)):
+    """
+    Decorator and wrapper around typer to mark a function as main.
+
+    For example:
+
+    @main
+    def run(...):
+        ...
+
+    Use of this decorator outside of the same module will not have
+    the intended effect.
+    """
     if named.get_module(func) == '__main__':
         app.command()(func)
         app()
